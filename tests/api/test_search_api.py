05760ba23b95cd893410fe12c48b355564c7fb86
Daniil Matafonov <danielmt.dm@gmail.com>
2022-10-06 14:32:49 +0300

    add allure annotations

--- tests/api/test_search_api.py
+++ tests/api/test_search_api.py
@@ -1,6 +1,10 @@
+import allure
+
 from helpers.url import BaseSession, requestSession
 
 
+@allure.description('Search')
+@allure.tag('api')
 def test_search_api():
     headers = {'User-Agent': 'PostmanRuntime/7.28.4'}
     response = requestSession().get(BaseSession.search_url, headers=headers, params="term=Java")
